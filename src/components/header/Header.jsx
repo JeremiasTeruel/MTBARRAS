de2832ebb4ed842b504154c7ce2a621b9d7b05c9
Jeremias Teruel <jteruel8@gmail.com>
2024-10-28 12:55:00 -0300

    change logo and add h1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,16 @@
 import './Header.css';
-import LOGO from '../../assets/clickmarketlogo.jpeg'
+import LOGO from '../../assets/logo_transparente.png'
+import Navbar from '../navbar/Navbar';
 
 
 export default function Header () {
     return (
         <header>
-            <img src={LOGO} alt="logo" className='logo' />
+            <div className='logo_h1_container'>
+                <img src={LOGO} alt="logo" className='logo' />
+                <h1>MT Barras</h1>
+            </div>
+            <Navbar />
         </header>
     )
 }
