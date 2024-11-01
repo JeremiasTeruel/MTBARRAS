d089e6679478ad799e55a8f9e32d7895d1811c4d
Jeremias Teruel <jteruel8@gmail.com>
2024-11-01 09:45:40 -0300

    add new aboutus component and give it style

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Carousel from './components/carousel/Carousel'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Services from './components/services/Services'
+import AboutUs from './components/aboutus/AboutUs'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header />
       <Carousel />
+      <AboutUs />
       <Services />
       <Footer/>
     </>
