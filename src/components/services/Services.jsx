c016fda2a022a5c4092ceba1a05ff927251531f2
Jeremias Teruel <jteruel8@gmail.com>
2025-01-29 12:34:04 -0300

    change prompt of cards and styled

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -7,9 +7,9 @@ import servicio_personalizado from '../../assets/servicio_personalizado.png'
 const Services = () => {
 
     const service = [
-        {id: '1', image: servicio_basico, description: 'Este es el servicio basico de MT BARRAS'},
-        {id: '2', image: servicio_avanzado, description: 'Este es el servicio avanzado de MT BARRAS'},
-        {id: '3', image: servicio_personalizado, description: 'Este es el servicio personalizado de MT BARRAS'}
+        {id: '1', image: servicio_basico, description: 'Servicio básico de MT BARRAS ofrecemos el conjunto base de tragos y aperitivos para todo tipo de eventos. Fernet, daikiri, aperol y cerveza.'},
+        {id: '2', image: servicio_avanzado, description: 'Con el servicio avanzado de MT BARRAS contarás con el servicio completo en tragos para tu evento. Fernet, daikiri, aperol, vodka, gin, cerveza y vino, siempre las mejores marcas y calidad asegurada.'},
+        {id: '3', image: servicio_personalizado, description: 'Este es el servicio personalizado de MT BARRAS. Con estsa opción contarás con nuestra asesoría profesional para encontrar la mejor opción en tragos y aperitivos para armar tu evento a tu propia medida.'}
     ]
 
 
