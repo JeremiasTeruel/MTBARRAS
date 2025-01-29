fcd6e4e63b272e681a917c8b37bfc1f8f406b6ef
Jeremias Teruel <jteruel8@gmail.com>
2025-01-29 12:32:55 -0300

    fix prompt and styled

--- src/components/aboutus/AboutUs.jsx
+++ src/components/aboutus/AboutUs.jsx
@@ -1,21 +1,22 @@
 import './AboutUs.css'
+import barras from '../../assets/barras.jpeg'
 
 const AboutUs = () => {
   return (
     <section id='aboutus' className='about_container'>
         <h1>🍍Sobre Nosotros🍍</h1>
-        <strong>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto tenetur saepe amet qui consectetur, placeat iure magni sint error illum aut tempore. Nesciunt temporibus, odio alias laboriosam autem vitae sint!
-            Tenetur eius quas harum repellat? Illo ex harum consequuntur dolor, minus sequi exercitationem et doloribus. Repellendus, modi dicta sapiente vel cupiditate tempore. Impedit, molestias commodi quae fuga labore nobis eum.
-            Aspernatur rerum eius placeat animi harum, ea voluptates, vel dolorum repellendus architecto nostrum maiores cum, ut dolor illo ab voluptas. Voluptatem consequuntur eveniet error aliquam harum ex est tempore. Voluptatum?
-            Nobis tempore dicta non deserunt voluptate recusandae perspiciatis qui, et sequi hic! Illum atque, accusamus et excepturi minus natus minima ipsum officia amet rem facere odio sapiente vero, voluptate velit.
-            Repudiandae, eum eius ipsa hic maiores commodi porro reiciendis tenetur a ea unde nemo amet rem nesciunt! Ipsum repellendus et saepe culpa ea debitis molestiae esse eius illum. Perferendis, minima!
-            Quae similique deserunt, omnis ipsa eos nesciunt quam numquam, ut corporis excepturi, necessitatibus odit suscipit? Rem optio repellendus perferendis placeat tenetur dolor sint, fuga eum quo provident quae velit maxime?
-            Dignissimos non, suscipit at est, voluptatum excepturi quam, officia culpa nam facilis consectetur qui maxime numquam laborum? Pariatur quae unde, obcaecati eos vitae molestias numquam natus quibusdam tenetur minus quasi!
-            Aliquam reprehenderit perferendis dicta possimus itaque laborum ea omnis explicabo, culpa labore, eos dolores eum nobis, molestias similique nemo hic! Placeat nemo minus facere quas, molestias incidunt! Cum, beatae expedita!
-            Odit, molestias quidem? Minima non voluptatem veniam rerum illum dolore totam distinctio sequi eveniet maiores a rem iure illo mollitia repellat officia dolor error asperiores, iusto quasi modi natus quis.
-            Sapiente eius impedit alias rerum. Error molestias, fuga maxime nostrum omnis officiis dolore, consequatur in laborum provident quisquam accusamus voluptatum numquam quos! Praesentium quibusdam eligendi ad dolorem atque quisquam itaque.
-        </strong>
+        <p>
+            En MT BARRAS, llevamos la fiesta a otro nivel con nuestras barras móviles diseñadas para hacer de cada evento una experiencia inolvidable. Nos especializamos en brindar un servicio de coctelería     premium para bodas, cumpleaños, eventos corporativos y celebraciones especiales.
+            Nuestro equipo de bartenders profesionales crea tragos únicos y personalizados, combinando creatividad, sabor y un servicio de primera. Con una barra elegante, ingredientes de calidad y una atención impecable, transformamos cualquier ocasión en un momento especial.
+            🎉 Tu evento, nuestra barra. ¡Brindemos por grandes momentos! 🍸
+        </p>
+
+        <div className='img_container'>
+          <img src={barras} alt="barras" />
+          <img src={barras} alt="barras" />
+          <img src={barras} alt="barras" />
+        </div>
+
     </section>
   )
 }
