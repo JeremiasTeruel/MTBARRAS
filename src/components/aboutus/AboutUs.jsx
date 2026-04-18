9a81fbff350492dc70c401d219a40ad4740ff0f1
Jeremias Teruel <98227790+JeremiasTeruel@users.noreply.github.com>
2026-04-18 20:24:59 -0300

    Update description in AboutUs component

--- src/components/aboutus/AboutUs.jsx
+++ src/components/aboutus/AboutUs.jsx
@@ -6,7 +6,7 @@ const AboutUs = () => {
     <section id='aboutus' className='about_container'>
         <h1>🍍Sobre Nosotros🍍</h1>
         <p>
-            En MT BARRAS, llevamos la fiesta a otro nivel con nuestras barras móviles diseñadas para hacer de cada evento una experiencia inolvidable. Nos especializamos en brindar un servicio de coctelería     premium para bodas, cumpleaños, eventos corporativos y celebraciones especiales.
+            Nos especializamos en brindar un servicio de coctelería premium para casamientos, cumpleaños, eventos corporativos y celebraciones especiales.
             Nuestro equipo de bartenders profesionales crea tragos únicos y personalizados, combinando creatividad, sabor y un servicio de primera. Con una barra elegante, ingredientes de calidad y una atención impecable, transformamos cualquier ocasión en un momento especial.
             🎉 Tu evento, nuestra barra. ¡Brindemos por grandes momentos! 🍸
         </p>
